2d094e1d1c6690c7ab5bcd31bc68d8a01a921e10
miya38-hub <miyacharge3803@gmail.com>
2026-02-18 14:11:27 +0000

    [Finish] 4-26

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create] , path_names: { new: 'sign_up' }
+  resources :books, only: [:new, :index, :show]
+
   resource :session
   resources :passwords, param: :token
   root to: "homes#top"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_18_103713) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_18_123934) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,6 +39,11 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_18_103713) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "books", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
@@ -53,6 +58,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_18_103713) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
